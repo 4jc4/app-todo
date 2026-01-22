e3ef841729793f6e79287832eb25fea6beb3ca65
Cardoso <ajca.dev@gmail.com>
2026-01-22 08:06:14 -0400

    add Badge component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import PlusIcon from './assets/icons/plus.svg?react'
 import SpinnerIcon from './assets/icons/spinner.svg?react'
 import TrashIcon from './assets/icons/trash.svg?react'
 import XIcon from './assets/icons/x.svg?react'
+import Badge from './components/badge'
 import Icon from './components/icon'
 
 export function App() {
@@ -23,9 +24,14 @@ export function App() {
         <Icon svg={TrashIcon} className="fill-green-base" />
         <Icon svg={CheckIcon} />
         <Icon svg={PlusIcon} />
-        <Icon svg={SpinnerIcon} animate />
+        <Icon svg={SpinnerIcon} />
         <Icon svg={PencilIcon} />
         <Icon svg={XIcon} />
+      </div>
+
+      <div>
+        <Badge variant="secondary">5</Badge>
+        <Badge variant="primary">2 de 5</Badge>
       </div>
     </div>
   )
