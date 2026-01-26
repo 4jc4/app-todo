ae7078ccd7a26818d47ab8c77fce084f103fc66b
Cardoso <ajca.dev@gmail.com>
2026-01-26 19:17:42 -0400

    add task list component

--- src/core-components/task-item.tsx
+++ src/core-components/task-item.tsx
@@ -10,7 +10,7 @@ import InputText from '../components/input-text'
 import Text from '../components/text'
 
 export default function TaskItem() {
-  const [isEditing, setIsEditing] = React.useState(true)
+  const [isEditing, setIsEditing] = React.useState(false)
 
   function handleEditTask() {
     setIsEditing(true)
